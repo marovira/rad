1ae79f18e62aef521de0b13a8d4fc33cf33e9a14
Mauricio A. Rovira Galvez <8482308+marovira@users.noreply.github.com>
2023-04-10 17:24:16 -0700

    [brief] Forgot to save radConfig.
    [detailed]

--- cmake/radConfig.cmake
+++ cmake/radConfig.cmake
@@ -1,3 +1,4 @@
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}")
 if (CMAKE_VERSION VERSION_GREATER_EQUAL 3.8.0)
     include(CMakeFindDependencyMacro)
     find_dependency(zeus 1.0.1 REQUIRED)
@@ -5,5 +6,5 @@ if (CMAKE_VERSION VERSION_GREATER_EQUAL 3.8.0)
     find_dependency(TBB 2021.8.0 REQUIRED)
 endif()
 
-include(${CMAKE_CURRENT_LIST_DIR}/coeusTargets.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/radTargets.cmake)
 
