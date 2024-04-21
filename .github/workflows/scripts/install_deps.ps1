328ca9bf212d83e571de6f2fbcaea814fb681281
Mauricio A. Rovira Galvez <8482308+marovira@users.noreply.github.com>
2024-04-21 13:02:03 -0700

    [brief] Forgot to change directories in the script for windows.
    [detailed]

--- .github/workflows/scripts/install_deps.ps1
+++ .github/workflows/scripts/install_deps.ps1
@@ -40,6 +40,7 @@ if(-not (Test-Path $c))
 
     git clone https://github.com/marovira/zeus --branch $zeus_ver --depth 1
     cmake -G "Visual Studio 17 2022" -S zeus -B zeus/build -DCMAKE_CXX_STANDARD=20 -DZEUS_INSTALL_TARGET=ON -DCMAKE_INSTALL_PREFIX=C:/deps/zeus -DCMAKE_PREFIX_PATH=C:/deps
+    cd ./zeus/build
     cmake --build . --config $env:BUILD_TYPE
     cmake --build . --target INSTALL --config $env:BUILD_TYPE
     cd ../..
