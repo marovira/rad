2062d4a4eb3e3607c02c19a96dd2116fb15639cf
Mauricio A. Rovira Galvez <8482308+marovira@users.noreply.github.com>
2024-04-21 11:30:41 -0700

    [brief] Adds a way to automatically retrieve versions in the scripts.
    [detailed]
    - The idea is to remove the need to update the workflow files (or the
      scripts) every time we update an SDK version. This should make it so
      the versions are pulled automatically.

--- .github/workflows/scripts/install_deps.sh
+++ .github/workflows/scripts/install_deps.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
 
 if [ ! -d ~/deps ]; then
+    if [[ $RUNNER_OS == "Windows" ]]; then
+        python .github/workflows/scripts/generate_versions.py
+    elif [[ $RUNNER_OS == "Linux" ]]; then
+        python3 .github/workflows/scripts/generate_versions.py
+    fi
+
+    # Read the necessary versions.
+    rad_deps=$(<~/rad_deps_version.txt)
+    fmt_ver=$(<~/rad_fmt_version.txt)
+    catch_ver="v$(<~/rad_catch_version.txt)"
+
     mkdir ~/deps
-    git clone https://github.com/marovira/rad_deps.git --branch 2.1.0 --depth 1
+    git clone https://github.com/marovira/rad_deps.git --branch $rad_deps --depth 1
     cd ./rad_deps/linux
     tar --use-compress-program=unzstd -xvf onnxruntime.tar.zst -C ~/deps
     tar --use-compress-program=unzstd -xvf opencv.tar.zst -C ~/deps
     tar --use-compress-program=unzstd -xvf TBB.tar.zst -C ~/deps
     cd ../..
 
-    git clone https://github.com/fmtlib/fmt.git --branch 10.2.0 --depth 1
+    git clone https://github.com/fmtlib/fmt.git --branch $fmt_ver --depth 1
     cmake -S fmt -B fmt/build -DCMAKE_CXX_STANDARD=20 -DFMT_INSTALL=ON -DFMT_TEST=OFF -DCMAKE_INSTALL_PREFIX=~/deps/fmt
     cd ./fmt/build
     make
     make install
     cd ../..
-    git clone https://github.com/catchorg/Catch2.git --branch v3.5.1 --depth 1
+    git clone https://github.com/catchorg/Catch2.git --branch $catch_ver --depth 1
     cmake -S Catch2 -B Catch2/build -DCMAKE_CXX_STANDARD=20 -DCATCH_INSTALL_DOCS=OFF -DCMAKE_INSTALL_PREFIX=~/deps/Catch2
     cd ./Catch2/build
     make
