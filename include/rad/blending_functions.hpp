6871bd1e932d725b3ac29ec141100e8021361181
Mauricio A. Rovira Galvez <8482308+marovira@users.noreply.github.com>
2025-04-09 11:25:39 -0700

    [brief] Add more blending functions.
    [detailed]
    - It's not uncommon to need a blending function for mixing images, so
      I've added the two Wyvill functions as they're the easiest compute.
    - Moves the mask blending function to the new header and changes the
      name.
    - Adds unit tests.

---
+++ include/rad/blending_functions.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <zeus/concepts.hpp>
+
+namespace rad
+{
+    template<std::floating_point T>
+    constexpr T soft_blend(T d, T r)
+    {
+        const auto d_6 = d * d * d * d * d * d;
+        const auto r_6 = r * r * r * r * r * r;
+        const auto d_4 = d * d * d * d;
+        const auto r_4 = r * r * r * r;
+        const auto d_2 = d * d;
+        const auto r_2 = r * r;
+
+        const auto a = (T{4} / T{9}) * (d_6 / r_6);
+        const auto b = (T{17} / T{9}) * (d_4 / r_4);
+        const auto c = (T{22} / T{9}) * (d_2 / r_2);
+
+        return T{1} - a + b - c;
+    }
+
+    template<std::floating_point T>
+    constexpr T soft_blend(T d)
+    {
+        return soft_blend(d, T{1});
+    }
+
+    template<std::floating_point T>
+    constexpr T wyvill_blend(T d, T r)
+    {
+        const auto g = T{1} - ((d * d) / (r * r));
+        return g * g * g;
+    }
+
+    template<std::floating_point T>
+    constexpr T wyvill_blend(T d)
+    {
+        return wyvill_blend(d, T{1});
+    }
+
+    template<std::floating_point T>
+    constexpr T sharp_mask_blend(T d)
+    {
+        const auto base = T{1} - (d * d * d);
+        auto g          = base * base * base;
+        g               = g * g;
+        g               = T{1} - g;
+        g               = g < T{1e-4} ? T{0} : g;
+        return g;
+    }
+} // namespace rad
---
+++ test/blending_functions_test.cpp
@@ -0,0 +1,66 @@
+#include <rad/blending_functions.hpp>
+
+#include <catch2/catch_template_test_macros.hpp>
+#include <zeus/float.hpp>
+
+template<typename T>
+consteval T epsilon()
+{
+    return static_cast<T>(0.00001f);
+}
+
+template<typename T>
+constexpr bool test_equal(T a, T b)
+{
+    return zeus::almost_equal<T>(a, b, epsilon<T>());
+}
+
+TEMPLATE_TEST_CASE("[blending_functions] - soft_blend", "[rad]", float, double)
+{
+    static constexpr TestType zero{0};
+    static constexpr TestType one{1};
+    static constexpr TestType half{0.5};
+
+    SECTION("Base function")
+    {
+        static constexpr TestType r{2};
+        REQUIRE(test_equal(rad::soft_blend<TestType>(zero, r), one));
+        REQUIRE(test_equal(rad::soft_blend<TestType>(one, r), half));
+        REQUIRE(test_equal(rad::soft_blend<TestType>(r, r), zero));
+    }
+
+    SECTION("Range 1")
+    {
+        REQUIRE(test_equal(rad::soft_blend<TestType>(zero), one));
+        REQUIRE(test_equal(rad::soft_blend<TestType>(half), half));
+        REQUIRE(test_equal(rad::soft_blend<TestType>(one), zero));
+    }
+}
+
+TEMPLATE_TEST_CASE("[blending_functions] - wyvill_blend", "[rad]", float, double)
+{
+    static constexpr TestType zero{0};
+    static constexpr TestType one{1};
+
+    SECTION("Base function")
+    {
+        static constexpr TestType r{2};
+        REQUIRE(test_equal(rad::wyvill_blend<TestType>(zero, r), one));
+        REQUIRE(test_equal(rad::wyvill_blend<TestType>(r, r), zero));
+    }
+
+    SECTION("Range 1")
+    {
+        REQUIRE(test_equal(rad::wyvill_blend<TestType>(zero), one));
+        REQUIRE(test_equal(rad::wyvill_blend<TestType>(one), zero));
+    }
+}
+
+TEMPLATE_TEST_CASE("[blending_functions] sharp_mask_blend", "[rad]", float, double)
+{
+    static constexpr TestType zero{0};
+    static constexpr TestType one{1};
+
+    REQUIRE(test_equal(rad::sharp_mask_blend<TestType>(zero), zero));
+    REQUIRE(test_equal(rad::sharp_mask_blend<TestType>(one), one));
+}
